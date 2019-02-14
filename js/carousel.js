7f0ee502482b69db0aacdd8723495d61f9ef12eb
Gabriel Osborne <osbornegabriel@gmail.com>
2019-02-14 18:59:02 -0500

    Create constants for track and slides, write stories for carousel

---
+++ js/carousel.js
@@ -0,0 +1,9 @@
+const track = document.querySelector('.carousel__track');
+const slides = Array.from(track.children);
+
+// console.log(track);
+// console.log(slides);
+
+// when I click left, move slides to the left
+// when I click right, move slides to the right
+// when I click on the nav indicators, move to that slide
