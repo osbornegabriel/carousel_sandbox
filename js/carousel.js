d81f51d01b88509736bda42b935ad12eb078bfee
Gabriel Osborne <osbornegabriel@gmail.com>
2019-02-14 19:10:32 -0500

    Create consts for buttons, dots, and slideWidth

--- js/carousel.js
+++ js/carousel.js
@@ -1,8 +1,12 @@
 const track = document.querySelector('.carousel__track');
 const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
 
-// console.log(track);
-// console.log(slides);
+const slideWidth = slides[0].getBoundingClientRect().width;
+// console.log(slideWidth);
 
 // when I click left, move slides to the left
 // when I click right, move slides to the right
